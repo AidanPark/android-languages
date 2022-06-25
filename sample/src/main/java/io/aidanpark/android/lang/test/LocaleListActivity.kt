a2fdb075bbe9f1ecb8530911fc2e459b9880a141
Aidan Park <j14458ed@gmail.com>
2022-06-26 00:26:42 +0900

    package name refactoring

---
+++ sample/src/main/java/io/aidanpark/android/lang/test/LocaleListActivity.kt
@@ -0,0 +1,29 @@
+package io.aidanpark.android.lang.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import io.aidanpark.android.lang.test.theme.AndroidLocaleTheme
+
+
+class LocaleListActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLocaleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LocalesScreen()
+                }
+            }
+        }
+    }
+}
+
+
